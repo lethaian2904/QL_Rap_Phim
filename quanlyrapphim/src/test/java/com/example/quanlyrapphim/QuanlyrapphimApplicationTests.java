7cbe6a1baf07fbfc47eb4242b155c8d78e610e1b
DuyLee01 <duyle7111@gmail.com>
2023-11-14 11:27:56 +0700

    Tao project backend

---
+++ quanlyrapphim/src/test/java/com/example/quanlyrapphim/QuanlyrapphimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.quanlyrapphim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuanlyrapphimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
